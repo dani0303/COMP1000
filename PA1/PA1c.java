015a5a93b01e083891d7a8967102ed0e7c3cb975
rodger2003 <48264113+dani0303@users.noreply.github.com>
2022-09-15 16:44:05 -0400

    Create PA1c.java

---
+++ PA1/PA1c.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PA1c 
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		
+		System.out.printf("1. Java %n2. Unit Tests %n3. JUnit Tests %n4. ");
+	}
+}
